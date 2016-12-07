fec0941aff65821bea219b0bba339e32e87248bf
Eric Hirshfield <erichirshfield@Erics-MacBook-Air-2.local>
2016-12-06 23:23:39 -0800

    started player constructor function for player creation

---
+++ player.js
@@ -0,0 +1,19 @@
+function Player(name, age, occupation) {
+    this.name = name;
+    this.age = age;
+    this.occupation = occupation;
+    this.strength = function() {
+
+    }
+
+    this.intelligence = function() {
+
+    }
+
+    this.health = function() {
+        
+
+    }
+}
+
+module.exports = Player
